20ffb8087a0301ca6ac1adbd7286a264fc02fd67
thiagomedinacc <thiago.medina@acad.pucrs.br>
2021-06-07 15:36:56 -0300

    add sprite nitro, corrige coliders, corrige direcao, add freio e marcha re

--- Assets/Scripts/CarControle.cs
+++ Assets/Scripts/CarControle.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class CarControle : MonoBehaviour
 {
 
+    private int qtdNitro = 1;
     public float potencia = 12f;
     public float handling = -100f;
     public float aceleracao = 0.004f;
     public float velMax;
 
     bool inercia = true;
+    bool inerciaRe = true;
 
-    public float velocidade = 0.7f;
+    public float velocidade = 0.1f;
+
+    public float marchaRe = 0.1f;
 
     Rigidbody2D rb;
    
@@ -27,11 +31,18 @@ public class CarControle : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-       // if (Input.GetKey("space")) { // Controles de drift.
-        //    rb.velocity = RightVelocity()/2 + ForwardVelocity()*1.5f; //+ ForwardVelocity();
-       // }
+
+       if (Input.GetKey("space")) { // Controles de freio
+            inerciaRe = false;
+             if (marchaRe < velMax){
+               marchaRe += aceleracao;
+            }
+            rb.AddForce(transform.up * potencia * (marchaRe/-2f) );
+          
+        }
 
         rb.velocity = ForwardVelocity() + RightVelocity() * 0.5f; // Para nao deslizar
+        
 
         if (Input.GetButton("Acelera")){
             inercia = false;
@@ -43,17 +54,22 @@ public class CarControle : MonoBehaviour
         }
         if (Input.GetButtonUp("Acelera")) {
             inercia = true;
-            rb.drag = 1.7f;
+            rb.drag = 1.3f;
         }
-        if (velocidade > 0.7f & inercia) {
-            velocidade -= 0.02f; // fator de volta a velocidade minima
-        }
+        
 
-       // Vector2 parado = new Vector2(0.01f, 0.01f);
-        if (rb.velocity.magnitude >= 0.2f){
+        if (velocidade > 0.1f & inercia) {
+               velocidade -= 0.01f; // fator de volta a velocidade minima
+            }
+        
+        if (marchaRe > 0.1f & inerciaRe) {
+            marchaRe -= 0.01f; // fator de volta a velocidade minima (da marcha ré)
+        }
+       
+        if (rb.velocity.magnitude >= 0.3f){
             rb.angularVelocity = Input.GetAxis("Horizontal") * handling;
         }
-        //}
+        
 
         
     }
@@ -67,4 +83,24 @@ public class CarControle : MonoBehaviour
      Vector2 RightVelocity(){
         return transform.right * Vector2.Dot(rb.velocity,transform.right);
     }
+
+
+   // void OnCollisionEnter2D(Collision2D colisor) {
+        //if (colisor.gameObject.tag == "Nitro") {
+          //  Debug.Log("carro encostou");
+           // Destroy(nitro);
+        //}
+    //}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        if (col.gameObject.tag == "Nitro") {
+            qtdNitro +=1;
+        }
+        
+        
+
+    }
+
 }
